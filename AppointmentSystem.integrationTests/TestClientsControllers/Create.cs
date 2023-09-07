dd4a74c984b08f222930d83a2e0dbb3614305abf
pavlodenshchyk <bloody.squirr3l@gmail.com>
2023-09-07 13:53:17 +0300

    add tests on Create and Delete Client

--- AppointmentSystem.integrationTests/TestClientsControllers/Create.cs
+++ AppointmentSystem.integrationTests/TestClientsControllers/Create.cs
@@ -1,6 +1,52 @@
+using System.Net;
+using System.Net.Http.Json;
+using appointmentSystem.Controllers.Features.Clients;
+using appointmentSystem.Data;
+using appointmentSystem.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
 namespace AppointmentSystem.integrationTests.TestClientsControllers;
 
-public class Create
+public class CreateClientTests
 {
+    private HttpClient _httpClient;
+    private string? _databaseName = "CreateClientTest";
+
+    [SetUp]
+    public void Setup()
+    {
+        var factory = new TestingWebApplicationFactory(_databaseName);
+        _httpClient = factory.CreateClient();
+    }
     
+    [Test]
+    public async Task CreateClient_ClientIsValid_Returns200_AndSamePhoneClientIsBadRequest()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(_databaseName).Options;
+        await using var dbContext = new AppDbContext(options);
+        var phone = "Phone";
+        var response = await _httpClient.PostAsJsonAsync("/api/clients", new CreateClientController.CreateClientCommand
+        {
+            Name = "Name",
+            Phone = phone
+        });
+        var responseSamePhone = await _httpClient.PostAsJsonAsync("/api/clients", new CreateClientController.CreateClientCommand
+        {
+            Name = "Name",
+            Phone = phone
+        });
+        
+        var responseString = await response.Content.ReadAsStringAsync();
+        var responseSamePhoneString = await responseSamePhone.Content.ReadAsStringAsync();
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(responseString, Contains.Substring("Name"));
+            Assert.That(responseString, Contains.Substring($"{phone}"));
+            Assert.That(dbContext.Clients.Count(), Is.EqualTo(1));
+            Assert.That(responseSamePhone.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            Assert.That(responseSamePhoneString, Contains.Substring($"A client with the phone {phone} already exists."));
+        });
+    }
 }
--- AppointmentSystem.integrationTests/TestClientsControllers/Delete.cs
+++ AppointmentSystem.integrationTests/TestClientsControllers/Delete.cs
@@ -1,6 +1,60 @@
+using System.Net;
+using appointmentSystem.Data;
+using appointmentSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
 namespace AppointmentSystem.integrationTests.TestClientsControllers;
 
-public class Delete
+public class DeleteClientTests
 {
+    private HttpClient _httpClient;
+    private string? _databaseName = "DeleteClientTests";
     
+    [SetUp]
+    public void Setup()
+    {
+        var factory = new TestingWebApplicationFactory(_databaseName);
+        _httpClient = factory.CreateClient();
+    }
+
+    [Test]
+    public async Task DeleteClient_ClientDoesntExist_Returns404()
+    {
+        var response = await _httpClient.GetAsync($"/api/clients/{Guid.NewGuid()}");
+        
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+    
+    [Test]
+    public async Task DeleteClient_ClientExists_ReturnsNoContentAndShouldBeNull()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(_databaseName).Options;
+        await using var dbContext = new AppDbContext(options);
+        var clientId = await AddClientToDatabase();
+
+        var response = await _httpClient.DeleteAsync($"/api/clients/{clientId}");
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
+            Assert.That(dbContext.Clients.Find(clientId), Is.Null);
+        });
+    }
+
+    private async Task<Guid> AddClientToDatabase()
+    {
+        var clientId = Guid.NewGuid();
+        var client = new Client
+        {
+            Id = clientId,
+            Name = "Test Name",
+            Phone = "Test Phone"
+        };
+        var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(_databaseName).Options;
+        await using var dbContext = new AppDbContext(options);
+        await dbContext.Clients.AddAsync(client);
+        await dbContext.SaveChangesAsync();
+        return clientId;
+    }
 }
